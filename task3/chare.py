139a91535d0fbc5d21ebedc018e5b7d9e4e5f336
ukorzun <vovakorzun96@gmail.com>
2023-11-19 19:09:42 +0300

    implementation of tasks

--- task3/chare.py
+++ task3/chare.py
@@ -2,8 +2,28 @@ import random
 from queue import Queue
 
 
-def bfs(field, start, end):
-    m, n = len(field), len(field[0])
+def generate_random_field(rows, cols, land_percentage=0.3):
+    field = [[0 if random.random() > land_percentage else 1 for _ in range(cols)] for _ in range(rows)]
+    return field
+
+
+def print_field(field):
+    for row in field:
+        for cell in row:
+            print('X' if cell == 1 else ' ', end=' ')
+        print()
+
+
+def is_valid_move(x, y, rows, cols, visited, field):
+    return 0 <= x < rows and 0 <= y < cols and (x, y) not in visited and field[x][y] == 0
+
+
+def get_neighbors(x, y):
+    return [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+
+
+def find_shortest_path(field, start, end):
+    rows, cols = len(field), len(field[0])
     queue = Queue()
     queue.put((start, [start]))
     visited = {start}
@@ -14,50 +34,33 @@ def bfs(field, start, end):
             return path
 
         for nx, ny in get_neighbors(x, y):
-            if is_valid_move(nx, ny, m, n, visited):
+            if is_valid_move(nx, ny, rows, cols, visited, field):
+                new_path = path + [(nx, ny)]
+                if (nx, ny) == end:
+                    return new_path
+                queue.put(((nx, ny), new_path))
                 visited.add((nx, ny))
-                if field[nx][ny] == 0 or (nx, ny) == end:
-                    queue.put(((nx, ny), path + [(nx, ny)]))
 
     return None
 
 
-def generate_field(m, n):
-    field = [[0 if random.random() > 0.3 else 1 for _ in range(n)] for _ in range(m)]
-    return field
-
-
-def print_field(field):
-    for row in field:
-        print(' '.join('X' if cell == 1 else ' ' for cell in row))
-
-
-def is_valid_move(x, y, m, n, visited):
-    return 0 <= x < m and 0 <= y < n and (x, y) not in visited
-
-
-def get_neighbors(x, y):
-    return [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-
-
 def main():
     try:
-        m = int(input("Введите количество строк поля (M): "))
-        n = int(input("Введите количество столбцов поля (N): "))
+        rows = int(input("Введите количество строк поля (M): "))
+        cols = int(input("Введите количество столбцов поля (N): "))
         start = tuple(int(input(f"Введите координату {coord} начальной точки A: ")) for coord in ('X', 'Y'))
         end = tuple(int(input(f"Введите координату {coord} конечной точки B: ")) for coord in ('X', 'Y'))
     except ValueError:
         print("Пожалуйста, введите целые числа.")
         return
 
-    field = generate_field(m, n)
+    field = generate_random_field(rows, cols)
     print("\nСгенерированное поле:")
     print_field(field)
 
-    path = bfs(field, start, end)
+    path = find_shortest_path(field, start, end)
 
     print(f"\nКратчайший путь от {start} до {end}: {path}" if path else "\nПуть не найден.")
-
 
 if __name__ == '__main__':
     main()
