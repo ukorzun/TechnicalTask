0a285451177edd143de9c15614a5276f01cf7112
ukorzun <vovakorzun96@gmail.com>
2023-11-21 16:36:03 +0300

    updates

--- task1/pages/wikipedia_page.py
+++ task1/pages/wikipedia_page.py
@@ -21,52 +21,69 @@ logger = logging.getLogger(__name__)
 
 
 class WikipediaProgrammingLanguagesPage(BasePage):
-
     PROGRAMMING_TABLE_XPATH = (By.XPATH, "//table[.//caption[contains(text(), 'Programming languages used')]]")
 
     def navigate_to_page(self):
         self.driver.get(self.url)
 
-    def wait_for_table(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.PROGRAMMING_TABLE_XPATH)
+    def wait_for_element(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located(locator)
+        )
+
+    def wait_for_elements(self, parent_element, locator, timeout=10):
+        return WebDriverWait(parent_element, timeout).until(
+            EC.presence_of_all_elements_located(locator)
         )
 
 
     def extract_table_data(self):
-        table = self.driver.find_element(*self.PROGRAMMING_TABLE_XPATH)
-        rows = table.find_elements(By.XPATH, "//tr[position() > 1]")
+        column_names = {
+            "websites": 0,
+            "popularity": 1,
+            "frontend": 2,
+            "backend": 3
+        }
+
+        def extract_valid_popularity_text(popularity_column):
+            match = re.search(r'[\d,.\[\]]+', popularity_column.text.strip())
+            popularity_value = match.group() if match else ""
+            popularity_value = re.sub(r'\.', '', popularity_value)
+            return popularity_value
+
+        table = self.wait_for_element(self.PROGRAMMING_TABLE_XPATH)
+        rows = self.wait_for_elements(table, (By.XPATH, ".//tr[position() > 1]"))
 
         data = []
 
-        for index, row in enumerate(rows, start=1):
-            columns = row.find_elements(By.XPATH, "td")
+        for row in rows:
+            columns = self.wait_for_elements(row, (By.XPATH, "td"))
 
-            if len(columns) < 4:
-                logger.warning(f"Skipping row {index}: {columns[0].text.strip()} - Insufficient columns")
+            if len(columns) < len(column_names):
+                logger.warning(f"Skipping row: {columns[0].text.strip()} - Insufficient columns")
                 continue
 
-            websites = columns[0].text.strip()
+            websites, popularity_text, frontend, backend = (
+                columns[column_names["websites"]].text.strip(),
+                extract_valid_popularity_text(columns[column_names["popularity"]]),
+                columns[column_names["frontend"]].text.strip(),
+                columns[column_names["backend"]].text.strip()
+            )
 
-            match = re.search(r'\d{1,3}(?:[.,]\d{3})*(?:\.\d*)?(?:\D|$)', columns[1].text.strip())
-
-            if not match:
-                logger.warning(f"Skipping row {index}: {websites} - No valid popularity value found")
+            if not websites or not popularity_text:
                 continue
-
-            popularity_text = re.sub(r'[.,]', '', match.group())
-
-            frontend = columns[2].text.strip()
-            backend = columns[3].text.strip()
 
             try:
-                popularity = int(popularity_text)
-                data.append(WebsiteInfo(websites, popularity, frontend, backend))
-                logger.warning(f"Added: {websites}, {popularity}, {frontend}, {backend}")
+                popularity = int(popularity_text.replace(",", "").replace("[", "").replace("]", ""))
             except ValueError:
-                logger.warning(f"Skipping row {index}: {websites} - Invalid popularity value: {popularity_text}")
+                logger.warning(f"Skipping row: {websites} - Invalid popularity value: {popularity_text}")
+                continue
 
-        else:
-            logger.warning("Table data extraction completed.")
+            data.append({
+                "websites": websites,
+                "popularity": popularity,
+                "frontend": frontend,
+                "backend": backend
+            })
 
         return data
---
+++ task1/tests/test_programming_languages.py
@@ -0,0 +1,42 @@
+import pytest
+
+from task1.pages.wikipedia_page import WikipediaProgrammingLanguagesPage
+
+
+class TestUniqueVisitors:
+
+    @pytest.mark.parametrize("popularity_threshold",
+                             [10 ** 7, 1.5 * 10 ** 7, 5 * 10 ** 7, 10 ** 8, 5 * 10 ** 8, 10 ** 9, 1.5 * 10 ** 9])
+    def test_popularity_threshold(self, popularity_threshold, chrome_driver):
+        with chrome_driver:
+            wikipedia_page = WikipediaProgrammingLanguagesPage(chrome_driver)
+            wikipedia_page.navigate_to_page()
+
+            websites_info = wikipedia_page.extract_table_data()
+            errors = self.check_popularity_threshold(websites_info, popularity_threshold)
+
+            if errors:
+                for error in errors:
+                    print(error)
+                raise AssertionError(f"Test failed with the following errors:\n{', '.join(errors)}")
+
+    def check_popularity_threshold(self, websites_info, popularity_threshold):
+        formatted_errors = []
+
+        for website_info in websites_info:
+            website_name = website_info.get('websites')
+            frontend = website_info.get('frontend')
+            backend = website_info.get('backend')
+            popularity = self.clean_numeric_string(website_info.get('popularity'))
+
+            if popularity is not None and popularity < popularity_threshold:
+                error_message = f"{website_name} (Frontend:{frontend}|Backend:{backend}) has {popularity} unique visitors per month. (Expected more than {popularity_threshold})"
+                formatted_errors.append(error_message)
+
+        return formatted_errors
+
+    def clean_numeric_string(self, value):
+        try:
+            return int(float(str(value).replace(',', '')))
+        except ValueError:
+            return None
